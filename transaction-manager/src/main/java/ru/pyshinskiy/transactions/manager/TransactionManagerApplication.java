b4248e16d21b393cffb6acce4cdc26231ca813fe
Pavel Pyshinskiy <p.pyshinskiy@gmail.com>
2021-10-10 14:33:30 +0700

    initial commit

---
+++ transaction-manager/src/main/java/ru/pyshinskiy/transactions/manager/TransactionManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.pyshinskiy.transactions.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionManagerApplication.class, args);
+    }
+
+}
---
+++ transaction-manager/src/test/java/ru/pyshinskiy/transactions/manager/TransactionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pyshinskiy.transactions.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
